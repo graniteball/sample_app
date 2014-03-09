3704c147bb146ff5f45aa66e2a6773cf799fa827
Alan J. Gilbert <ajgilbert@aol.com>
2014-03-09 10:01:07 -0400

    Completed chapter 7 exercises 1-3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,16 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+
+			describe "after submission" do
+				before{ click_button submit }
+
+				it {should have_selector('title', text: 'Sign Up')}
+				it {should have_content('The form contains')}
+				it {should have_content('error')}
+				it {should have_css('div.alert.alert-error')}
+			end
+
 		end
 
 		describe "with valid information" do
